804aa2eecaf2fe5dadde64ad87370d839807e69c
Xuebin Dong <xudong@redhat.com>
2014-06-03 16:56:31 +0800

    REV: adjust the structure of topic

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,14 @@
 $(function($){
   // show sample data
   $('.sampledata').on('click', function() {
-    $('#blackboard').children('.topic').hide();
+    var that = $(this);
+    that.siblings().hide();
+    that.css('background', 'transparent');
+    that.animate({'width': 968, 'height': 568}, 500, function() {
+      that.children('.sourcecode').show();
+    });
+    that.children('.heading').animate({'font-size': 30}, 500);
+    that.children('.description').animate({'font-size': 20}, 500);
   });
 
   // 
