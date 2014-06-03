9b8794c655a8e3519eb3e9b95608287d74ffb5e9
Xuebin Dong <xudong@redhat.com>
2014-06-03 18:02:28 +0800

    REV: add scroll bar for source code

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,10 +2,9 @@ $(function($){
   // show sample data
   $('.sampledata').on('click', function() {
     var that = $(this);
-    that.siblings().hide();
-    that.css('background', 'transparent');
+    that.siblings().hide().end().css('background', 'transparent');
     that.animate({'width': 968, 'height': 568}, 500, function() {
-      that.children('.sourcecode').show();
+      that.children('.sourcecode').show().jScrollPane();
     });
     that.children('.heading').animate({'font-size': 30}, 500);
     that.children('.description').animate({'font-size': 20}, 500);
