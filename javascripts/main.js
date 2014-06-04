9a72e944dfbe869c563989e843e10ab46f676224
Xuebin Dong <xudong@redhat.com>
2014-06-04 15:43:40 +0800

    REV: add textarea for source code

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,13 +4,13 @@ $(function($){
     var that = $(this);
     that.siblings().hide().end().css('background', 'transparent');
     that.animate({'width': 968, 'height': 568}, 500, function() {
-      that.children('.sourcecode').show().jScrollPane();
+      that.children('.sourcecode').show();
       $('.commandBtn').show();
     });
     that.children('.heading').animate({'font-size': 30}, 500);
     that.children('.description').animate({'font-size': 20}, 500);
   });
 
-  // 
+  //
 
 });
