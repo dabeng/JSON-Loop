4735499075306b3133382f43818c0cac266936ce
Xuebin Dong <xudong@redhat.com>
2014-06-04 10:48:43 +0800

    REV: add command buttons for topic

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,6 +5,7 @@ $(function($){
     that.siblings().hide().end().css('background', 'transparent');
     that.animate({'width': 968, 'height': 568}, 500, function() {
       that.children('.sourcecode').show().jScrollPane();
+      $('.commandBtn').show();
     });
     that.children('.heading').animate({'font-size': 30}, 500);
     that.children('.description').animate({'font-size': 20}, 500);
